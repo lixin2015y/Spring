c432ac64ba8d62a59c64ac8b7340c5290f41b63d
lixin2015y <lixin2015y@163.com>
2020-05-29 17:30:07 +0800

    深入理解java虚拟机

---
+++ javaNote/src/main/java/com/lee/concurrent/Cas.java
@@ -0,0 +1,16 @@
+package com.lee.concurrent;
+
+import org.junit.Test;
+import sun.misc.Unsafe;
+
+public class Cas {
+
+
+    @Test
+    public void test() {
+
+        Unsafe unsafe = Unsafe.getUnsafe();
+
+    }
+
+}
