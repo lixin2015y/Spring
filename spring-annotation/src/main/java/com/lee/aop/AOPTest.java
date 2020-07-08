97304b77eedf114ae7c2cf83fb7b987976e283a5
lixin2015y <lixin2015y@163.com>
2020-07-08 17:31:03 +0800

    Spring源码设计

--- spring-annotation/src/main/java/com/lee/aop/AOPTest.java
+++ spring-annotation/src/main/java/com/lee/aop/AOPTest.java
@@ -1,15 +1,20 @@
 package com.lee.aop;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AOPTest {
 
 
     @Test
     public void test() {
-        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainConfigOfAOP.class);
-        final Calculator calculator = context.getBean(Calculator.class);
-        System.out.println(calculator.div(1, 1));
+//        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainConfigOfAOP.class);
+//        final Calculator calculator = context.getBean(Calculator.class);
+//        System.out.println(calculator.div(1, 1));
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
     }
 }
