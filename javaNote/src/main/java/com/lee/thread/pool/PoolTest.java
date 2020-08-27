b110ae340c7a586ffbfae6478cfd038afd665d6b
lixin2015y <lixin2015y@163.com>
2020-08-28 07:46:01 +0800

    Redis.md

--- javaNote/src/main/java/com/lee/thread/pool/PoolTest.java
+++ javaNote/src/main/java/com/lee/thread/pool/PoolTest.java
@@ -1,21 +1,14 @@
 package com.lee.thread.pool;
 
-import org.junit.Test;
-
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
 public class PoolTest {
 
 
 
-    @Test
-    public void test() {
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE,
-                TimeUnit.SECONDS, new LinkedBlockingDeque<>());
-    }
+//    @Test
+//    public void test() {
+//        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE,
+//                TimeUnit.SECONDS, new LinkedBlockingDeque<>());
+//    }
 
 
 }
--- spring-annotation/src/main/java/com/lee/controller/BookController.java
+++ spring-annotation/src/main/java/com/lee/controller/BookController.java
@@ -1,9 +1,25 @@
 package com.lee.controller;
 
+import org.junit.Test;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BookController {
+
+    public static void main(String[] args) {
+        Integer a = 123456;
+        String b = String.valueOf(a);
+        StringBuilder stringBuilder = new StringBuilder(b);
+        StringBuilder result = stringBuilder.reverse();
+        System.out.println(result);
+    }
+
+    @Test
+    public void test(){
+        int a = 1;
+        Integer b = 1;
+        System.out.println(a == b);
+    }
 
 
 }
