79aca29e06f71095cad2dfa6916318824136bf1d
lixin2015y <lixin2015y@163.com>
2021-10-16 23:00:09 +0800

    查找最小子数组问题

--- javaNote/src/main/java/com/lee/algorithm/problem/MinimumSizeSubarraySum.java
+++ javaNote/src/main/java/com/lee/algorithm/problem/MinimumSizeSubarraySum.java
@@ -28,10 +28,9 @@ public class MinimumSizeSubarraySum {
 
         // 表示窗口中元素加和
         int sum = 0;
-        // 数组取值，注意边界问题
-        while (l < arr.length && r + 1 < arr.length) {
-            if (sum < s) {
-                
+        while (l < arr.length) {
+            // 此处放置数组越界
+            if (sum < s && r + 1 < arr.length) {
                 r = r + 1;
                 sum += arr[r];
             } else {
@@ -41,38 +40,10 @@ public class MinimumSizeSubarraySum {
             }
             if (sum >= s) {
                 // 判断
-                sum = Math.min(r - l + 1, result);
+                result = Math.min(r - l + 1, result);
             }
         }
 
-        if (result == arr.length + 1) {
-            return 0;
-        }
-
-        return result;
-    }
-
-
-    static int minSubArray(int[] arr, int s) {
-        int l = 0;
-        int r = -1;
-        int result = arr.length + 1;
-        int sum = 0;
-        while (l < arr.length && l + 1 < arr.length && r + 1 < arr.length) {
-            if (sum > s) {
-                // 最短已经找到，将l向右移
-                sum = sum - arr[l];
-                l = l + 1;
-            } else {
-                r = r + 1;
-                sum = sum + arr[r];
-            }
-
-            if (sum > s) {
-                result = Math.min(sum, r - l + 1);
-            }
-
-        }
         if (result == arr.length + 1) {
             return 0;
         }
@@ -83,9 +54,7 @@ public class MinimumSizeSubarraySum {
     public static void main(String[] args) {
         int[] arr = {2, 3, 1, 2, 4, 3};
         int s = 7;
-        int i = minSubArray(arr, s);
         int j = minSubArrLength(arr, s);
-        System.out.println(i);
         System.out.println(j);
     }
 
