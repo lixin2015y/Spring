2504a13d6f43a2113eb1a1d8e6ca7d4c7ace7c18
lixin2015y <lixin2015y@163.com>
2021-10-18 20:07:08 +0800

    数组算法

---
+++ javaNote/src/main/java/com/lee/algorithm/problem/DeleteElementReturnLeavedCount.java
@@ -0,0 +1,16 @@
+package com.lee.algorithm.problem;
+
+/**
+ * 给定一个数组，一个目标元素，返回数组中和目标元素相同的个数，
+ * 给定[1,2,3,4,5,6,2] target=2 返回2 ，并且arr保证[2,2,,,,,,]
+ */
+public class DeleteElementReturnLeavedCount {
+
+    public static void solve(int[] arr, int target) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ javaNote/src/main/java/com/lee/algorithm/problem/MoveZeroToArrayLast.java
@@ -0,0 +1,84 @@
+package com.lee.algorithm.problem;
+
+import com.lee.algorithm.utils.ArrayUtils;
+import com.sun.org.apache.regexp.internal.RE;
+
+import javax.swing.*;
+import java.util.Arrays;
+
+/**
+ * 将所有0移动到数组末尾,保持其他元素相对位置不变
+ * <p>
+ * <p>
+ * <p>
+ * 2、
+ * 3、
+ */
+public class MoveZeroToArrayLast {
+
+    // 1、遍历数组，找到所有非0的填充到另一数组，剩下的元素补0
+    public static int[] move1(int[] arr) {
+        int[] result = new int[arr.length];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                result[index++] = arr[i];
+            }
+        }
+        for (int i = index; i < result.length; i++) {
+            result[i] = 0;
+        }
+
+        return result;
+    }
+
+    // 双指针移动，不使用额外的内存空间,时间复杂度为O(n)，空间复杂度为O(1)级别
+    public static int[] move2(int[] arr) {
+
+        int nextZeroIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[nextZeroIndex++] = arr[i];
+            }
+        }
+        for (int i = nextZeroIndex; i < arr.length; i++) {
+            arr[i] = 0;
+        }
+        return arr;
+    }
+
+    // 双指针交换思路，
+    public static int[] move3(int[] arr) {
+        int zeroIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                // 这里避免所有都是非0元素，自己和自己交换
+                if (i != zeroIndex) {
+                    swap(zeroIndex, i, arr);
+                }
+                zeroIndex++;
+            }
+        }
+        return arr;
+    }
+
+    private static void swap(int i, int j, int[] arr) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 30, 90, 0, 2, 3, 1, 0, 4, 0};
+        int[] result = move1(arr);
+        System.out.println(Arrays.toString(result));
+        int[] result2 = move2(arr);
+        System.out.println(Arrays.toString(result2));
+
+        int[] result3 = move3(arr);
+        System.out.println(Arrays.toString(result3));
+
+    }
+
+}
