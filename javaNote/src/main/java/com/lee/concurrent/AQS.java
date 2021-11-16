3076914044fe62300a248bc28963c6613cec7d1d
lixin2015y <lixin2015y@163.com>
2021-11-17 07:46:45 +0800

    AQS

---
+++ javaNote/src/main/java/com/lee/concurrent/AQS.java
@@ -0,0 +1,28 @@
+package com.lee.concurrent;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AQS {
+    private static int count;
+    private static ReentrantLock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        new Thread(AQS::count).start();
+        new Thread(AQS::count).start();
+        new Thread(AQS::count).start();
+        System.out.println(count);
+    }
+
+    private static void count() {
+        try {
+            lock.lock();
+            for (int i = 0; i < 10000; i++) {
+                count++;
+            }
+        } finally {
+            lock.unlock();
+        }
+
+
+    }
+}
