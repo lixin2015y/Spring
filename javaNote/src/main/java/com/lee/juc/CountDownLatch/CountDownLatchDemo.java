2e8f8e5b76cd9c058433d1edf762f9ad228c37de
lixin2015y <=>
2021-12-10 17:31:57 +0800

    zookeeper.md

--- javaNote/src/main/java/com/lee/juc/CountDownLatch/CountDownLatchDemo.java
+++ javaNote/src/main/java/com/lee/juc/CountDownLatch/CountDownLatchDemo.java
@@ -1,9 +1,7 @@
 package com.lee.juc.CountDownLatch;
 
-import com.sun.jmx.snmp.internal.SnmpOutgoingRequest;
-import org.omg.PortableServer.THREAD_POLICY_ID;
-
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 六个同学离开教室之后，班长锁门
@@ -13,13 +11,18 @@ public class CountDownLatchDemo {
 
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(6);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             new Thread(() -> {
                 System.out.println(Thread.currentThread().getName() + "走了");
+                try {
+                    TimeUnit.SECONDS.sleep(4);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 countDownLatch.countDown();
             }).start();
         }
-        countDownLatch.await();
+        countDownLatch.await(2, TimeUnit.SECONDS);
         System.out.println(Thread.currentThread().getName() + "班长锁门了");
     }
 }
