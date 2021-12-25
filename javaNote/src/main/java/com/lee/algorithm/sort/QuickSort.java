21669dc2cb1391a6eb04e67ab441e0d8a57b1a83
lixin2015y <=>
2021-12-25 09:50:20 +0800

    设计链表

--- javaNote/src/main/java/com/lee/algorithm/sort/QuickSort.java
+++ javaNote/src/main/java/com/lee/algorithm/sort/QuickSort.java
@@ -1,5 +1,7 @@
 package com.lee.algorithm.sort;
 
+import java.util.TreeSet;
+
 /**
  * @className: QuickSort
  * @author: li xin
@@ -8,6 +10,13 @@ package com.lee.algorithm.sort;
 public class QuickSort {
 
     public static void main(String[] args) {
-
+        TreeSet<Integer> set = new TreeSet();
+        set.add(1);
+        set.add(10);
+        set.add(11);
+        set.add(7);
+        set.add(6);
+        set.add(7);
+        System.out.println(set);
     }
 }
