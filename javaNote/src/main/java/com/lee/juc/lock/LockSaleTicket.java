5029c409990081933694e8b0e8faf6346c3d66f5
lixin2015y <lixin2015y@163.com>
2022-07-10 18:20:47 +0800

    锁中断

--- javaNote/src/main/java/com/lee/juc/lock/LockSaleTicket.java
+++ javaNote/src/main/java/com/lee/juc/lock/LockSaleTicket.java
@@ -1,55 +1,55 @@
 package com.lee.juc.lock;
 
+import sun.security.krb5.internal.Ticket;
+
 import java.util.concurrent.locks.ReentrantLock;
 
-/**
- * @className: LockSaleTicket
- * @author: li xin
- * @date: 2021-10-16
- **/
-class Ticket {
-    private int number = 30;
-
-    private ReentrantLock lock = new ReentrantLock();
-
-    public void sale() {
-        lock.lock();
-        try {
-            if (number > 0) {
-                number--;
-                System.out.println(Thread.currentThread().getName() + "卖票,剩下" + number + "张");
-            }
-        }finally {
-            lock.unlock();
-        }
-
-    }
-}
 
 public class LockSaleTicket {
-    public static void main(String[] args) {
-        Ticket ticket = new Ticket();
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock(true);
 
         Thread aaThread = new Thread(() -> {
-            for (int i = 0; i < 40; i++) {
-                ticket.sale();
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + "开始执行");
+            try {
+                for (int i = 0; i < 10; i++) {
+                    Thread.sleep(1000);
+                    System.out.println(Thread.currentThread().getName());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            lock.unlock();
+            System.out.println(Thread.currentThread().getName() + "释放锁");
         }, "AA");
 
         Thread bbThread = new Thread(() -> {
-            for (int i = 0; i < 40; i++) {
-                ticket.sale();
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + "开始执行");
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName() + "开始执行");
             }
+            lock.unlock();
+            System.out.println(Thread.currentThread().getName() + "释放锁");
         }, "BB");
-
         Thread ccThread = new Thread(() -> {
-            for (int i = 0; i < 40; i++) {
-                ticket.sale();
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + "开始执行");
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName());
             }
+            lock.unlock();
+            System.out.println(Thread.currentThread().getName() + "释放锁");
         }, "CC");
 
+
         aaThread.start();
+        Thread.sleep(10);
         bbThread.start();
+        Thread.sleep(1000);
         ccThread.start();
+        System.out.println("123123");
+        bbThread.interrupt();
     }
 }
