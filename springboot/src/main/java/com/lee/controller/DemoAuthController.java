b3150a5acf278dbfebae5b2dfb3fd210b231cac5
== <lixin@baoyinxiaofei.com>
2022-07-27 23:14:17 +0800

    111

--- springboot/src/main/java/com/lee/controller/DemoAuthController.java
+++ springboot/src/main/java/com/lee/controller/DemoAuthController.java
@@ -4,6 +4,9 @@ import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class DemoAuthController {
 
@@ -12,5 +15,24 @@ public class DemoAuthController {
         JSONObject data = new JSONObject();
         data.put("hello", "world");
         return data;
+    }
+
+    List<List<MessageDto>> list = new ArrayList<>();
+    @GetMapping("generate")
+    public String generate() {
+        ArrayList<MessageDto> l = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            l.add(new MessageDto("1502240110" + i));
+        }
+        list.add(l);
+        return list.size() + "";
+    }
+
+    class MessageDto {
+        private String mobile;
+
+        public MessageDto(String mobile) {
+            this.mobile = mobile;
+        }
     }
 }
