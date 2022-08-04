1c1899ad46718e1f42224c02171dec168dbb62ef
== <lixin@baoyinxiaofei.com>
2022-08-04 17:15:29 +0800

    redis锁实现。。。

--- springboot/src/main/java/com/lee/redission/RedissionTest.java
+++ springboot/src/main/java/com/lee/redission/RedissionTest.java
@@ -4,12 +4,15 @@ import com.lee.App;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.redisson.Redisson;
+import org.redisson.api.RBloomFilter;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.rmi.ConnectIOException;
+import java.util.Base64;
 import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -90,8 +93,42 @@ public class RedissionTest {
             }
 
 
-        },"t2").start();
+        }, "t2").start();
         countDownLatch.countDown();
         new Scanner(System.in).nextLine();
+    }
+
+
+
+    @Test
+    public void test3() {
+        RBloomFilter<Long> blackList = redissonClient.getBloomFilter("blackList");
+        blackList.tryInit(80000, 0.03);
+        long phone = 15022401101l;
+        for (int i = 0; i < 100000; i++) {
+            blackList.add((long) i + phone);
+        }
+
+        boolean contains = blackList.contains(15022401105l);
+        System.out.println(contains);
+        blackList.dump();
+
+
+    }
+
+    @Test
+    public void test4() {
+        RBloomFilter<Long> blackList = redissonClient.getBloomFilter("blackList");
+        System.out.println("blackList.contains(15022401125l) = " + blackList.contains(15022401125l));
+        blackList.add(1l);
+        System.out.println("blackList.contains(15022401125l) = " + blackList.contains(1l));
+
+    }
+
+    @Test
+    public void test5() {
+        RBloomFilter<Long> blackList = redissonClient.getBloomFilter("blackList");
+        blackList.dump();
+
     }
 }
