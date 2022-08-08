a61fd7370fca64c49d92eb5493b77d733aa1e265
== <lixin@baoyinxiaofei.com>
2022-08-08 16:59:28 +0800

    1111

--- spring-annotation/src/main/java/com/lee/tx/UserDao.java
+++ spring-annotation/src/main/java/com/lee/tx/UserDao.java
@@ -1,9 +1,17 @@
 package com.lee.tx;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.stereotype.Repository;
+import sun.security.krb5.internal.PAData;
 
 import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class UserDao {
@@ -21,6 +29,21 @@ public class UserDao {
         jdbcTemplate.update(sql, id);
     }
 
+    public List<Map<String, Object>> selectAll() {
+        List<Map<String, Object>> list = new ArrayList<>();
+        jdbcTemplate.query("select * from test", new RowCallbackHandler() {
+            @Override
+            public void processRow(ResultSet resultSet) throws SQLException {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                Map<String, Object> param = new HashMap<>();
+                param.put("id", id);
+                param.put("name", name);
+                list.add(param);
+            }
+        });
+        return list;
+    }
 
 
 }
--- spring-annotation/src/main/java/com/lee/tx/UserService.java
+++ spring-annotation/src/main/java/com/lee/tx/UserService.java
@@ -5,6 +5,8 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -12,15 +14,21 @@ public class UserService {
     @Resource
     UserDao userDao;
 
+    @Resource
+    UserService userService;
 
+
+    @Transactional
     public void insert(Integer id,String name){
         userDao.insert(id, name);
-        delete(333);
+        userService.delete(333);
+        int a = 1 / 0;
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void delete(Integer id) {
-        userDao.delete(id);
-        int a = 1 / 0;
+        userDao.insert(id + 1, "-----");
+        List<Map<String, Object>> maps = userDao.selectAll();
+        System.out.println("maps = " + maps);
     }
 }
