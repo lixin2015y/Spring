9d5a4780b51c33e03ec955653b0b142c3c3976de
lixin08_dxm <lixin08@duxiaoman.com>
2023-11-18 14:58:52 +0800

    临床分型

--- learning/src/main/java/weka/randomforest/MainRandomForestTraining.java
+++ learning/src/main/java/weka/randomforest/MainRandomForestTraining.java
@@ -2,6 +2,7 @@ package weka.randomforest;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -22,9 +23,11 @@ public class MainRandomForestTraining {
 
     public static void main(String[] args) throws Exception {
 
+        int seed = 12345;
         // 训练语料文件
         File inputFile = new File(arffFilePath);
-        Classifier randomForest = new RandomForest();
+        RandomForest randomForest = new RandomForest();
+        randomForest.setSeed(seed);
         ArffLoader atf = new ArffLoader();
         atf.setFile(inputFile);
         // 读入训练文件
@@ -34,26 +37,31 @@ public class MainRandomForestTraining {
         randomForest.buildClassifier(instancesTrain);
 
         // 参数一为模型保存文件，classifier4为要保存的模型
-
         SerializationHelper.write(modelPath, randomForest);
 
     }
 
     @Test
     public void test2() throws Exception {
-        Classifier classifier8 = (Classifier) weka.core.SerializationHelper.read(modelPath);
-        Instances instances = generatePopularInstance();
+        RandomForest classifier8 = (RandomForest) weka.core.SerializationHelper.read(modelPath);
+        //        Instances instances = generatePopularInstance();
+        classifier8.setSeed(12345);
+        File inputFile = new File(arffFilePath);
+        ArffLoader atf = new ArffLoader();
+        atf.setFile(inputFile);
+        // 读入训练文件
+        Instances instances = atf.getDataSet();
+        instances.setClassIndex(0);
+
         for (int i = 0; i < instances.size(); i++) {
-            System.out.println(
-                    "instancesTest.instance(i) = " + instances.instance(i) + "====" + instances.instance(i).classValue()
-                            + "===" + classifier8.classifyInstance(instances.instance(i)));
+            System.out.println(classifier8.classifyInstance(instances.instance(i)));
         }
     }
 
     private Instances generatePopularInstance() {
         //set attributes
         ArrayList<Attribute> attributes = new ArrayList<>();
-        attributes.add(new Attribute("Cluster"));
+        attributes.add(new Attribute("Cluster", Arrays.asList("0", "1")));
         attributes.add(new Attribute("WBC"));
         attributes.add(new Attribute("Renal_insufficiency"));
         attributes.add(new Attribute("Solid_malignant_tumor"));
@@ -61,10 +69,10 @@ public class MainRandomForestTraining {
 
         // 1,28.85,0,0,0
         Instance instance = new DenseInstance(attributes.size());
-        instance.setValue(1, 28.85);
+        instance.setValue(1, 19.12);
         instance.setValue(2, 0);
         instance.setValue(3, 0);
-        instance.setValue(4, 0);
+        instance.setValue(4, 1);
         //set instances
         Instances instances = new Instances("repo_popular", attributes, 0);
         instances.setClassIndex(0);
