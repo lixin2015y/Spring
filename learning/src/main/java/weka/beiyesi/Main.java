e48ba3b33c14932d06253fbacc16dd9edda72de4
lixin08_dxm <lixin08@duxiaoman.com>
2023-12-14 19:20:32 +0800

    生理分型

--- learning/src/main/java/weka/beiyesi/Main.java
+++ learning/src/main/java/weka/beiyesi/Main.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import weka.classifiers.Classifier;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.meta.AdaBoostM1;
-import weka.classifiers.trees.RandomForest;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
@@ -17,7 +16,7 @@ import weka.core.converters.ArffLoader;
 public class Main {
 
     private static String arffPath = "/Users/lixin08_dxm/IdeaProjects/Spring/ARDS生理分型.arff";
-    private static String arffPathTest = "/Users/lixin08_dxm/IdeaProjects/Spring/ARDS生理分型-test.arff";
+    private static String arffPathTest = "/Users/lixin08_dxm/IdeaProjects/Spring/ARDS生理分型.arff";
 
     private static String modelPath = "/Users/lixin08_dxm/IdeaProjects/Spring/ARDS生理分型.model";
 
@@ -39,6 +38,30 @@ public class Main {
     @Test
     public void test() throws Exception {
         NaiveBayes classifier8 = (NaiveBayes) weka.core.SerializationHelper.read(modelPath);
+
+//        ArrayList<Attribute> attributes = new ArrayList<>();
+//        attributes.add(new Attribute("Cluster", Arrays.asList("1", "2", "3")));
+//        attributes.add(new Attribute("D0_PO2_FiO2_Ratio", Arrays.asList("99", "150", "250", "301")));
+//        attributes.add(new Attribute("D0_Lac", Arrays.asList("1", "2", "3", "4")));
+//        attributes.add(new Attribute("D0_pH", Arrays.asList("7.2", "7.4", "7.5", "7.6")));
+//        attributes.add(new Attribute("D0_PO2", Arrays.asList("49", "51", "81", "111")));
+//        attributes.add(new Attribute("D0_PCO2", Arrays.asList("29", "31", "41", "51")));
+//        attributes.add(new Attribute("D0_HCO3", Arrays.asList("17", "19", "23", "27")));
+//        attributes.add(new Attribute("Respiratory_support", Arrays.asList("1", "2", "3")));
+//        Instances instances = new Instances("repo_popular", attributes, 0);
+//        instances.setClassIndex(0);
+//        Instance instance = new DenseInstance(8);
+//        instance.setDataset(instances);
+//        instance.setValue(1, "250");
+//        instance.setValue(2, "3");
+//        instance.setValue(3, "7.4");
+//        instance.setValue(4, "81");
+//        instance.setValue(5, "31");
+//        instance.setValue(6, "19");
+//        instance.setValue(7, "2");
+//        double v = classifier8.classifyInstance(instance);
+//        System.out.println(v);
+
         File inputFile = new File(arffPathTest);
         ArffLoader atf = new ArffLoader();
         atf.setFile(inputFile);
@@ -46,15 +69,13 @@ public class Main {
         Instances instances = atf.getDataSet();
         instances.setClassIndex(0);
         int successCount = 0;
-        for (int i = 0; i < instances.size(); i++) {
-            double d = classifier8.classifyInstance(instances.instance(i));
-            if (d == instances.get(i).classValue() + 1) {
+        for (Instance instance : instances) {
+            double v = classifier8.classifyInstance(instance);
+            if (v == instance.classValue()) {
                 successCount++;
             }
-            System.out.println(d + "===" + instances.get(i).classValue());
         }
-        System.out.println(successCount + "====" + instances.size());
-
+        System.out.println(successCount + "=" + instances.size());
     }
 
     @Test
